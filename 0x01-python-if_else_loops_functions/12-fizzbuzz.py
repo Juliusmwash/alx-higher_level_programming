a69ba48aa7e370db3a4f0833e918d447f18b92b4
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-08 03:38:53 +0300

    Querry 13

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FIZZBUZZ", end=' ')
+        elif i % 3 == 0:
+            print("FIZZ", end=' ')
+        elif i % 5 == 0:
+            print("BUZZ", end=' ')
+        else:
+            print("{:d} ".format(i), end='')
+
+fizzbuzz()
