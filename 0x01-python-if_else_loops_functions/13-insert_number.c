aa5abf61d0b6d495f25cd6f174f01ddb0509d9c0
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-09 17:27:41 +0300

    Querry 14

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,37 +7,39 @@
  * Return: newly created node or null it there is no any
  */
 
-listint_t *insert_node_helper(listint_t **head, int number, listint_t **tmp2)
+listint_t *insert_node_helper(listint_t **head, int number)
 {
-	*tmp2 = malloc(sizeof(listint_t));
-	if (*tmp2 == NULL)
-		return (NULL);
-	(*tmp2)->n = number;
-
 	if (*head == NULL)
 	{
-		(*tmp2)->next = NULL;
-		*head = *tmp2;
-		return (*tmp2);
+		tmp2 = malloc(sizeof(listint_t));
+		if (tmp2 == NULL)
+			return (NULL);
+		tmp2->n = number;
+		tmp2->next = NULL;
+		*head = tmp2;
+		return (tmp2);
 	}
 	else if ((*head)->n > number)
 	{
-		(*tmp2)->next = *head;
-		*head = *tmp2;
-		return (*tmp2);
+		tmp2 = malloc(sizeof(listint_t));
+		if (tmp2 == NULL)
+			return (NULL);
+		tmp2->n = number;
+		tmp2->next = *head;
+		*head = tmp2;
+		return (tmp2);
 	}
 	else if ((*head)->next == NULL)
 	{
-		(*tmp2)->next = NULL;
-		(*head)->next = *tmp2;
-		return (*tmp2);
+		tmp2 = malloc(sizeof(listint_t));
+		if (tmp2 == NULL)
+			return (NULL);
+		tmp2->n = number;
+		tmp2->next = NULL;
+		(*head)->next = tmp2;
+		return (tmp2);
 	}
-	else
-	{
-		free(*tmp2);
-		*tmp2 = NULL;
-	}
-	return (*tmp2);
+	return (NULL);
 }
 
 /**
@@ -49,26 +51,24 @@ listint_t *insert_node_helper(listint_t **head, int number, listint_t **tmp2)
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *tmp1;
+	listint_t *tmp1, *tmp2;
 
-	insert_node_helper(head, number, &tmp2);
-	if (tmp2 != NULL)
+	tmp1 = insert_node_helper(head, number);
+	if (tmp1 != NULL)
 		return (tmp2);
 
 	tmp1 = *head;
 	while (tmp1 != NULL)
 	{
+		tmp2 = malloc(sizeof(listint_t));
+		if (tmp2 == NULL)
+			return (NULL);
+		tmp2->n = number;
 		if (tmp1->next->n < number)
 		{
 			tmp1 = tmp1->next;
 			if (tmp1->next  == NULL)
 			{
-				listint_t *tmp2;
-
-				tmp2 = malloc(sizeof(listint_t));
-				if (tmp2 == NULL)
-					return (NULL);
-				tmp2->n = number;
 				tmp2->next = NULL;
 				tmp1->next = tmp2;
 				return (tmp2);
@@ -76,16 +76,12 @@ listint_t *insert_node(listint_t **head, int number)
 		}
 		else
 		{
-			listint_t *tmp2;
-
-			tmp2 = malloc(sizeof(listint_t));
-			if (tmp2 == NULL)
-				return (NULL);
-			tmp2->n = number;
 			tmp2->next = tmp1->next;
 			tmp1->next = tmp2;
 			return (tmp2);
 		}
+		free(tmp2);
+
 	}
 	return NULL;
 }
