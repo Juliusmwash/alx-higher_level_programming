91d0a9d0c92bf2b1a15de0307301efca3e35d002
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-09 14:56:40 +0300

    Edit 13-insert_number.c

--- insertToSortedList/13-insert_number.c
+++ insertToSortedList/13-insert_number.c
@@ -1,6 +1,7 @@
-listint_t *insert_node(listint_t **head, int number)
+
+listint_t *insert_node_helper(listint_t **head, listint_t *tmp2)
 {
-	listint_t *tmp1, *tmp2, *tmp3;
+	listint_t *tmp1;
 
 	tmp1 = *head;
 	tmp2 = malloc(sizeof(listint_t));
@@ -21,7 +22,45 @@ listint_t *insert_node(listint_t **head, int number)
 		return (tmp2);
 	}
 	else
+	{
 		free(tmp2);
+		tmp2 = NULL;
+	}
+	return (tmp2);
+}
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *tmp1, *tmp2;
+
+	/*
+	tmp1 = *head;
+	tmp2 = malloc(sizeof(listint_t));
+	if (tmp2 == NULL)
+		return (NULL);
+	tmp2->n = number;
+
+	if ((*head)->n > number)
+	{
+		tmp2->next = *head;
+		*head = tmp2;
+		return (tmp2);
+	}
+	else if ((*head)->next == NULL)
+	{
+		tmp2->next = NULL;
+		(*head)->next = tmp2;
+		return (tmp2);
+	}
+	else
+		free(tmp2);
+	*/
+
+	tmp2 = insert_node_helper(head, tmp2);
+	if (tmp2 != NULL)
+		return (tmp2);
+
+	tmp1 = *head;
 	while (tmp1 != NULL)
 	{
 		tmp2 = malloc(sizeof(listint_t));
