5db0f8f33798ab008d77766140094462d49082c1
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-15 02:13:11 +0300

    Solve querry 10

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max_int = None
+    count = 0
+
     if my_list:
-        max_int = 0
         for i in my_list:
-            if i > max_int:
+            if count == 0:
+                max_int = i
+                count += 1
+            elif i > max_int:
                 max_int = i
         return max_int
