cbd2fc56ccc745d5aebbef01c4081133d73c42b6
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-14 22:32:13 +0300

    Solve querry 7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-        print("")
+    """Print a matrix of integers"""
+    for row in matrix:
+        for j in range(len(row)):
+            if j != len(row) - 1:
+                print("{:d} ".format(row[j]), end='')
+            else:
+                print("{:d}".format(row[j]))
