b20fdf170ac4abf6a7d0521d96d853960e65e339
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-15 02:01:07 +0300

    Solve querry 10

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_int = None
+    if my_list:
+        max_int = 0
+        for i in my_list:
+            if i > max_int:
+                max_int = i
+        return max_int
