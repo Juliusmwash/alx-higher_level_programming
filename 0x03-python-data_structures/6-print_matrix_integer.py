5015962616cd851cd738624455be703582c726ca
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-14 22:54:31 +0300

    Solve querry 7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers"""
-    if not matrix:
-        print("")
     for row in matrix:
+        if not row:
+            print("Julius")
         for j in range(len(row)):
             if j != len(row) - 1:
                 print("{:d}".format(row[j]), end=' ')
