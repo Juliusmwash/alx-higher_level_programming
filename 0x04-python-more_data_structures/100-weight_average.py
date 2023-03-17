a55e7c03675f52f3dea4804641c697ecfada8e38
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-17 15:35:13 +0300

    Solve advance 1

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,10 +2,12 @@
 def weight_average(my_list=[]):
     sum = 0
     avrg_sum = 0
+
     if len(my_list) == 0:
         return 0;
     for i in range(len(my_list)):
         mult = 1
+
         for j in range(len(my_list[i])):
             mult *= my_list[i][j]
             if j == len(my_list[i]) - 1:
