8774d83af8bdfb0e26e5462f26ac050fe837564c
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-17 15:30:13 +0300

    Solve advance 1

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    """Return the weighted average of all integers in a list of tuples."""
-    if not isinstance(my_list, list) or len(my_list) == 0:
-        return (0)
-
-    avg = 0
-    size = 0
-    for tup in my_list:
-        avg += (tup[0] * tup[1])
-        size += tup[1]
-    return (avg / size)
+    sum = 0
+    avrg_sum = 0
+    if len(my_list) == 0:
+        return 0;
+    for i in range(len(my_list)):
+        mult = 1
+        for j in range(len(my_list[i])):
+            mult *= my_list[i][j]
+            if j == len(my_list[i]) - 1:
+                sum += mult
+                avrg_sum += my_list[i][j]
+    return sum / avrg_sum
