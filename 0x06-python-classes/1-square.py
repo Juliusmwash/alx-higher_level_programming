7cfd50464ebedd24878807baf10ba2eacae6f919
Juliusmwash <cutejuliusmwash@gmail.com>
2023-03-29 02:24:11 +0300

    Solve task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Define a class 'Square' """
+
+
+class Square:
+    """ Empty 'Square' class """
+    def __init__(self, size):
+        """ Constructor """
+        self.__size = size
