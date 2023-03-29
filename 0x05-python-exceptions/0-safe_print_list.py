d23d3f5d73e1e72f7dce62a1cdeff03ce66581cf
Juliusmwash <cutejuliusmwash@gmail.com>
2023-03-30 02:44:24 +0300

    Fix an error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[j]), end="")
         except IndexError as err:
-            print("{}".format(err))
+            print("")
             return length
         count += 1
     print("")
