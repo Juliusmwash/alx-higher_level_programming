c0766438cec677a12389f4deb539fb086539717d
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-05 00:10:19 +0300

    Solve first task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" Addition of integers """
+"""
+Define add_integer function
+This functions adds two integer
+If float is given it is casted to int before addition
+"""
 
 
 def add_integer(a, b=98):
-    """ Function for adding and returning sum of two integers
-
-    Args:
+    """ Function for adding two integers
         a (int) or (float): first parameter.
-        b (int) or (float): second parameter.
-    """
+        b (int) or (float): second parameter. """
     if not isinstance(a, int):
         if not isinstance(a, float):
             raise TypeError("a must be an integer")
