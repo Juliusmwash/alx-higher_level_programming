6f475aaf4e6519c15be737969b7682ed90bcf891
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-12 21:59:51 +0300

    Solve advanced task 1

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,15 +2,19 @@
 """ Defines class MyInt that inherits fron class int """
 
 
-class MyInt:
+class MyInt(int):
     """ reverses the comparison signs """
     def __init__(self, num):
         """ class MyInt constructor """
         self.num = num
 
-    def __eq__(self, value):
+    def __eq__(self, other):
         """ reverses the '==' sign """
-        return not self.num
+        return self.num != other
+
+    def __ne__(self, other):
+        """ reverses the '!=' sign """
+        return self.num == other
 
     def __str__(self):
         """ return an string """
