be93f07bb8b7850e1e0f05ee843f8df5d3a07ab3
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-13 14:59:03 +0300

    Solve task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Adds all arguments to a Python list, and then save them to a file """
+import json
+import sys
+import os
+
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
+
+check = 0
+argv_list = []
+if os.path.exists("add_item.json"):
+    argv_list2 = load_file("add_item.json")
+    for item in argv_list2:
+        argv_list.append(item)
+else:
+    print("file does not exist")
+for item in sys.argv:
+    if check:
+        argv_list.append(item)
+    else:
+        check = 1
+save_file(argv_list, "add_item.json")
