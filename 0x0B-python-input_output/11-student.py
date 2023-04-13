f31630232f2d5ddaaf08040bec845ca0ea5c603e
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-13 21:20:22 +0300

    Solve task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -23,6 +23,7 @@ class Student:
 
     def reload_from_json(self, json):
         """ Replaces all attributes of the Student instance """
-        self.first_name = json["first_name"]
-        self.last_name = json["last_name"]
-        self.age = json["age"]
+        if len(json) != 0:
+            self.first_name = json["first_name"]
+            self.last_name = json["last_name"]
+            self.age = json["age"]
