96507c449ffc5673e98bf65b42ade9e93b19ba01
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-16 03:13:59 +0300

    Solve task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,9 +21,42 @@ class Square(Rectangle):
     def size(self):
         """ returns the size of the square """
         return self.width
+
     @size.setter
     def size(self, value):
         """ sets value for size """
         Rectangle.validator("width", value, 0)
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ Updates class 'Square' """
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    if type(args[i]) == int:
+                        self.id = args[i]
+                elif i == 1:
+                    Rectangle.validator("width", args[i], 0)
+                    self.width = args[i]
+                    self.height = args[i]
+                elif i == 2:
+                    Rectangle.validator("x", 1, args[i])
+                    self.x = args[i]
+                elif i == 3:
+                    Rectangle.validator("y", 1, args[i])
+                    self.y = args[i]
+        else:
+            if 'id' in kwargs:
+                if type(kwargs['id']) == int:
+                    self.id = kwargs['id']
+            if 'size' in kwargs:
+                Rectangle.validator("width", kwargs['size'], 0)
+                self.width = kwargs['size']
+                self.height = kwargs['size']
+            if 'x' in kwargs:
+                Rectangle.validator("x", 1, kwargs['x'])
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                Rectangle.validator("y", 1, kwargs['y'])
+                self.y = kwargs['y']
