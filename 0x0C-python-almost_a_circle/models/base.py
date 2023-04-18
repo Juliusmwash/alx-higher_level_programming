17db14493981804624b2e858f5368ec3563be3ab
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-18 14:02:04 +0300

    Fix error on to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,6 +21,8 @@ class Base:
     def to_json_string(list_dictionaries):
         """ returns the JSON string representation of
         list_dictionaries """
+        if list_dictionaries is None:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
