0f53f61781dd831950b483257dc4d3529aa2ff7c
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-20 02:06:54 +0300

    Solve task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
