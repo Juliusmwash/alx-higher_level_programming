2d09d462cf90039fbfc691cbfdb0a0309e7d754e
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-19 10:29:05 +0300

    Solve task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,31 +7,3 @@ and database name.
 
 import sys
 import MySQLdb
-
-
-def list_states(username, password, database):
-    """
-    Connect to the MySQL server and retrieves all states
-    in ascending order
-    """
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    cursor = db.cursor()
-    query = "SELECT * FROM states ORDER BY id ASC"
-    cursor.execute(query)
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-    cursor.close()
-    db.close()
-
-
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-list_states(username, password, database)
