b9acae7b2f6801bc1fae873ff319b34919e0b59f
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-19 11:33:58 +0300

    Solve task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     query = (
             "SELECT * FROM states "
-            "WHERE name='{}' "
+            "WHERE name LIKE BINARY '{}%' "
             "ORDER BY id ASC".format(state_name)
             )
     cursor.execute(query)
