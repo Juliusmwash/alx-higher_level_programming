3a39a545ea5e0fe6468ebd75d54628627a2cf481
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-19 10:19:54 +0300

    Solve task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
+'''
+Lists all states from the database hbtn_0e_0_usa           It should take 3 arguments: mysql username,
+mysql password and database name.
+'''
 
 import MySQLdb
 from sys import argv
 
-'''
-Lists all states from the database hbtn_0e_0_usa
-It should take 3 arguments: mysql username,
-mysql password and database name.
-'''
 if __name__ == "__main__":
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
