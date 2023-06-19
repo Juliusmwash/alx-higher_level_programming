e52355a452f4f3dcd85fdd36779caae06d0196f4
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-19 10:57:50 +0300

    Solve task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N)
+from the database
+"""
+import MySQLdb
+import sys
+
+
+def get_states_starting_with_N(username, password, database):
+    """
+    Retrieves and displays states from the hbtn_0e_0_usa
+    database that have names starting with an uppercase 'N'.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Database name.
+
+    Returns:
+        None
+    """
+    db = MySQLdb.connect(user=username, passwd=password, db=database, port=3306, host='localhost')
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    get_states_starting_with_N(username, password, database)
