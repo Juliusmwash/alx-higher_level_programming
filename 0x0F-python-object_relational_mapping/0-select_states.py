084b9f3be11856cbc241307d38f2a9baddbeb409
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-19 10:25:49 +0300

    Solve task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""
+'''
 Lists all states from the database hbtn_0e_0_usa.
 should take 3 arguments: mysql username, mysql password
 and database name.
-"""
+'''
 
 import sys
 import MySQLdb
