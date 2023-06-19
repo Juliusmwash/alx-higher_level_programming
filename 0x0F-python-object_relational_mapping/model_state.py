1e0fa2fbbcc7e47908006acc5b73ad4eaf9b96ab
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-06-20 00:35:51 +0300

    Solve task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,10 +4,8 @@ Contains the class definition of a State and an instance
 Base = declarative_base()
 """
 import sys
-from model_state import Base, State
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 
 Base = declarative_base()
 
@@ -25,15 +23,3 @@ class State(Base):
         nullable=False
     )
     name = Column(String(128), nullable=False)
-
-username = sys.argv[1]
-password = sys.argv[2]
-db_name = sys.argv[3]
-create_file = 'mysql://{}:{}@localhost:3306/{}'.format(
-        username,
-        password,
-        db_name
-        )
-engine = create_engine(create_file)
-if __name__ == '__main__':
-    Base.metadata.create_all(engine)
