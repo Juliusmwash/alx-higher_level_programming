2e808398b3926044711b2cddedbb5968bb873cb3
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-24 00:06:10 +0300

    Solve task 16

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -6,160 +6,17 @@ contained in the database hbtn_0e_101_usa
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
+from relationship_state import State
 from relationship_city import City
 
-if __name__ == '__main__':
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: python3 101-relationship_states_cities_list.py <username> <password> <database_name>")
-        sys.exit(1)
-
-    # Get command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Create engine and bind session
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}')
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the database
-    states = session.query(State).order_by(State.id).all()
-
-    # Print the results
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print(f"\t{city.id}: {city.name}")
-
-    # Close the session
-    session.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from relationship_city import City
-
-if __name__ == '__main__':
-    # Get command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Create engine and bind session
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}')
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query the database
-    states = session.query(State).order_by(State.id).all()
-
-    # Print the results
-    for state in states:
-        print(f"{state.id}: {state.name}")
-        for city in state.cities:
-            print(f"\t{city.id}: {city.name}")
-
-    # Close the session
-    session.close()
+            print("    {}: {}".format(city.id, city.name))
