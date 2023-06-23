18d3cc8c2e206ae036d183fdd2c23b66b6bb9daa
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-23 23:35:57 +0300

    Solve task 15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,47 +1,20 @@
 #!/usr/bin/python3
-"""
-Creates the State "California" with the City "San Francisco"
-"""
+# Creates the State “California” with the City “San Francisco”
+# from the database hbtn_0e_100_usa.
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import ProgrammingError
-from relationship_state import Base, State
-from relationship_city import City
+from relationship_state import State
+from relationship_city import Base, City
 
 if __name__ == "__main__":
-    # Get the MySQL username, password, and database name
-    #  from command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-
-    # Create the connection string for the MySQL server
-    connection_string = (
-            f"mysql+mysqldb://{username}:{password}"
-            f"@localhost:3306/{dbname}")
-
-    # Create the engine and session
-    engine = create_engine(connection_string, pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    try:
-        # Check if the tables exist
-        engine.execute('SELECT 1 FROM states LIMIT 1')
-        engine.execute('SELECT 1 FROM cities LIMIT 1')
-
-        # Create the State "California"
-        california = State(name="California")
-        city = City(name="San Francisco")
-        california.cities.append(city)
-
-        # Add the State and City objects to the session and commit
-        session.add(california)
-        session.add(city)
-        session.commit()
-
-    except ProgrammingError as e:
-        error_msg = f"ERROR {e.orig.args[0]}: {e.orig.args[1]}"
-        print(error_msg)
+    session.add(City(name="San Francisco", state=State(name="California")))
+    session.commit()
