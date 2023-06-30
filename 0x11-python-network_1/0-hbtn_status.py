c4d9abb48085e4e7e6f5f2d88c12cd7223eed874
Juliusmwash <cutejuliusmwash@gmail.com>
2023-07-01 01:32:24 +0300

    Replace spaces with tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ with urllib.request.urlopen(url) as response:
 body_response = content.decode("utf-8")
 
 print("Body response:")
-print("    - type:", type(content))
-print("    - content:", content)
-print("    - utf8 content:", body_response)
+print("\t- type:", type(content))
+print("\t- content:", content)
+print("\t- utf8 content:", body_response)
