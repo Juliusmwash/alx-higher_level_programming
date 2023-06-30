c3fb9929f347eebdc0efd22eb6a43c86590d5bfe
Juliusmwash <cutejuliusmwash@gmail.com>
2023-07-01 01:09:55 +0300

    Solve advanced task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Backend technical challenge """
+import requests
+import sys
+
+def get_commits(repo, owner):
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    response = requests.get(url)
+    commits = response.json()
+
+    for commit in commits[:10]:
+        sha = commit.get("sha")
+        author_name = commit.get("commit", {}).get("author", {}).get("name")
+        print(f"{sha}: {author_name}")
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    get_commits(repo, owner)
