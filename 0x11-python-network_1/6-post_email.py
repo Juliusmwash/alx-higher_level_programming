837b2e6d1beec1f006ffdae2ceed8e56c1009b00
Juliusmwash <cutejuliusmwash@gmail.com>
2023-07-01 04:16:43 +0300

    Solve task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,7 +11,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    print("Your email is:", email)
     data = {"email": email}
     response = requests.post(url, data=data)
-    print("Response body:", response.text)
+    print("Your email is:", response)
