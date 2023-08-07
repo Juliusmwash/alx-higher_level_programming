af2ea69b1f86698bc0e5b7f034533475f697fbd4
Juliusmwash <cutejuliusmwash@gmail.com>
2023-08-07 09:05:57 +0300

    Solve task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $.getJSON("https://swapi-api.alx-tools.com/api/films/?format=json", function(data) {
+        var movies = data.results;
+        var list = $("#list_movies");
+
+        $.each(movies, function(index, movie) {
+            var listItem = $("<li>" + movie.title + "</li>");
+            list.append(listItem);
+        });
+    });
+});
+
